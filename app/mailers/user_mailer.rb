99f4f9c28f4e3b56488a9aa4620eb6c3450d50b4
Jesen92 <hjesenovic@gmail.com>
2016-12-07 13:18:48 +0100

    CloudConnect -> Cloud Connect

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -58,7 +58,7 @@ class UserMailer < ApplicationMailer
     @body = body
 
     if locale == "hr"
-      mail(to: @email,bcc: 'cloudconnect@megatrend.com', subject: @subject, template_path: 'user_mailer', template_name: 'contact_us_hr')
+      mail(to: @email,bcc: 'hrvoje.jesenovic@megatrend.com', subject: @subject, template_path: 'user_mailer', template_name: 'contact_us_hr')
       #mail(to: "cloudconnect@megatrend.com", subject: @subject, template_path: 'user_mailer', template_name: 'contact_us_hr')
     else
       mail(to: @email,bcc: 'cloudconnect@megatrend.com', subject: @subject, template_path: 'user_mailer', template_name: 'contact_us')
