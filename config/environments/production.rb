4ad1a427518e3b8a21f5a78c55af8b0d2aa01744
Jesen92 <hjesenovic@gmail.com>
2017-08-03 17:55:36 +0200

    promijenjen asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.asset_host = "http://mdmweb.herokuapp.com"
+  config.action_mailer.asset_host = "http://qradarmega.herokuapp.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :port           => ENV['MAILGUN_SMTP_PORT'],
